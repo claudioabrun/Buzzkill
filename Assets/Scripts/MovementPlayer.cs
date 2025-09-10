962677f15a9a0439c2b4dbed8b0569ab4eaafda3
claudioabrun <78451778+claudioabrun@users.noreply.github.com>
2025-09-10 03:42:41 -0500

    Script changing and Cameras
    Simplifying some of the scripts for easier editing, implementing camera, as well as adding sprites for the main character

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -4,40 +4,19 @@ using UnityEngine;
 
 public class MovementPlayer : MonoBehaviour
 {
-    public Camera sceneCamera;
-    public float moveSpeed;
-    public Rigidbody2D rb;
-
-    private Vector2 moveDirection;
-    private Vector2 mousePosition;
+    public float speed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
 
     void Update()
     {
-        ProcessInputs();
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-    }
-
-    void ProcessInputs()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        moveDirection = new Vector2(moveX, moveY).normalized;
-        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
-    }
-
-    void Move()
-    {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-
-        //Rotate the player
-        Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = aimAngle;
+        speedX = Input.GetAxisRaw("Horizontal") * speed;
+        speedY = Input.GetAxisRaw("Vertical") * speed;
+        rb.velocity = new Vector2(speedX, speedY);
     }
 }

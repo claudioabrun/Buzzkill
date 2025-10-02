823071db6e32fe5226f66078b2031de1a3f279f3
claudioabrun <78451778+claudioabrun@users.noreply.github.com>
2025-10-02 16:00:19 -0500

    I'm tired

--- Assets/Scripts/CloseGame.cs
+++ Assets/Scripts/CloseGame.cs
@@ -2,18 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CloseGame : MonoBehaviour
 {
+    [SerializeField] private string gameOverScene = "_GameOver";
+    [SerializeField] private int forceIndex = 2;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            QuitApplication();
+            Debug.Log("Collision detected with Player");
+            Debug.Log("Current Scene: " + SceneManager.GetActiveScene().name);
+            Debug.Log("Trying to load scene by name: " + gameOverScene);
+            Debug.Log("Trying to load scene by index: " + forceIndex);
+
+            SceneManager.LoadScene(forceIndex, LoadSceneMode.Single);
         }
-    }
-    private void QuitApplication()
-    {
-        Application.Quit();
     }
 }
--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -11,35 +11,56 @@ public class MovementPlayer : MonoBehaviour
 
     public Projectile projectilePrefab;
     public Transform LaunchOffset;
+
+    private bool isGameOver = false; // Flag to prevent unwanted restarts
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
+        // Movement
         speedX = speed;
         speedY = Input.GetAxisRaw("Vertical") * (speed + 5);
         rb.velocity = new Vector2(speedX, speedY);
 
+        // Shooting
         if (Input.GetKeyDown(KeyCode.J))
         {
             Instantiate(projectilePrefab, LaunchOffset.position, transform.rotation);
         }
-
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.CompareTag("Enemy"))
         {
+            // Player hits an enemy: destroy and restart scene
             Destroy(gameObject);
         }
+        else if (collision.gameObject.CompareTag("Hazard")) // Or CloseGame object
+        {
+            // Player hits a hazard: destroy and trigger GameOver
+            isGameOver = true;
+            Destroy(gameObject);
+            SceneManager.LoadScene("_GameOver");
+        }
     }
+
     private void OnDestroy()
     {
-        RestartCurrentScene();
+        // Only restart the scene if it wasn't a GameOver
+        if (!isGameOver)
+        {
+            RestartCurrentScene();
+        }
     }
+
     public void RestartCurrentScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
+
